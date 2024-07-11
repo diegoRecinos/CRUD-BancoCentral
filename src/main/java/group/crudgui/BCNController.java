57400142ab9aa691564bcb4584507a236c555ba0
diegoRecinos <00182122@uca.edu.sv>
2024-07-11 04:47:35 -0600

    fx:id declared in controller

--- src/main/java/group/crudgui/BCNController.java
+++ src/main/java/group/crudgui/BCNController.java
@@ -11,6 +11,49 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BCNController implements Initializable {
+    public TextField fieldTransaccionIDTarjeta;
+    public Button btnTarjetaDelete;
+    public Button btnTarjetaUpdate;
+    public Button btnTarjetaRead;
+    public Button btnTarjetaSave;
+    public TextField fieldTransacionDesc;
+    public DatePicker dtpickerTransaccionDate;
+    public TextField fieldTransaccionMonto;
+    public Button btnTransaccionSave;
+    public Button btnTransaccionRead;
+    public Button btnTransaccionUpdate;
+    public Button btnTransaccionDelete;
+    public TableView tableViewTarjeta;
+    public TableColumn colTarjetaID;
+    public TableColumn colTarjetaNumero;
+    public TableColumn colTarjetaFechaExp;
+    public TableColumn colTarjetaIDtipoTarjeta;
+    public TableColumn colTarjetaIDtipoFacilitador;
+    public TableColumn colTarjetaIDCliente;
+    public TableColumn colTransaccionIDCliente;
+    public TableColumn colTransaccionIDTarjeta;
+    public TableColumn colTransaccionMonto;
+    public TableColumn colTransaccionFecha;
+    public TableColumn colTransaccionDescripcion;
+    public TableColumn colTransaccionID;
+    public TableView tableViewTransaccion;
+    public TextField fieldTarjetaIDCliente;
+    public TextField fieldTarjetaNumber;
+    public DatePicker dtpickerTarjetaExpire;
+    public ComboBox comboBxTarjetaFacilitador;
+    public Button btnClienteDelete;
+    public Button btnClienteUpdate;
+    public Button btnClienteRead;
+    public Button btnClienteSave;
+    public TextField fieldClienteTel;
+    public TextField fieldClienteID;
+    public TextField fieldClienteAddress;
+    public TextField fieldClienteName;
+    public TableColumn colClienteTelefono;
+    public TableColumn colClienteDireccion;
+    public TableColumn colClienteNombre;
+    public TableColumn colClienteID;
+    public TableView tableViewCliente;
     @FXML
     private Label welcomeText;
 //    //private ComboBox<Facilitador> facilitador;
@@ -74,6 +117,19 @@ public class BCNController implements Initializable {
         colLastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         tableView.setItems(list);
     }
+    
+//    @FXML
+//    private void showClientes()
+//    {
+//        Query query = new Query();
+//        ObservableList<Cliente> list = query.getClienteList();
+//        columnID.setCellValueFactory(new PropertyValueFactory<>("id"));
+//        colFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+//        colMiddleName.setCellValueFactory(new PropertyValueFactory<>("middleName"));
+//        colLastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+//        tableView.setItems(list);
+//    }
+    
 
     @FXML
     private void addStudentFunction()
