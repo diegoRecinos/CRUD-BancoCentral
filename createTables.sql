692ddb627b2f42a1f837d593ac34e3b5900dcf81
Recinos <104780516+diegoRecinos@users.noreply.github.com>
2024-07-19 02:53:07 -0600

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,17 +1,15 @@
--- Creación de la tabla tipoTarjeta
 CREATE TABLE tipoTarjeta (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
 
--- Creación de la tabla FacilitadorTarjeta
+
 CREATE TABLE FacilitadorTarjeta (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
 
 
--- Creación de la tabla Cliente
 CREATE TABLE Cliente (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
@@ -20,13 +18,12 @@ CREATE TABLE Cliente (
     
 );
 
--- Creación de la tabla Tarjeta
+
 CREATE TABLE Tarjeta (
     id INT PRIMARY KEY,
     numero_tarjeta VARCHAR(20) NOT NULL,
     id_tipo_tarjeta INT,
-    fecha_expiracion DATE NOT NULL,++++++++++++++++++++++
-    +
+    fecha_expiracion DATE NOT NULL,
     id_facilitador_tarjeta INT,
     id_cliente INT,
     FOREIGN KEY (id_tipo_tarjeta) REFERENCES tipoTarjeta(id),
@@ -34,7 +31,7 @@ CREATE TABLE Tarjeta (
     FOREIGN KEY (id_cliente) REFERENCES Cliente(id)
 );
 
--- Creación de la tabla Transaccion
+
 CREATE TABLE Transaccion (
     id INT PRIMARY KEY,
     fecha_compra DATE NOT NULL,
