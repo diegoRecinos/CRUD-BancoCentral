b33af9c6e7516a817c40f5cbe12b190b3a7c1dd3
diegoRecinos <00182122@uca.edu.sv>
2024-07-22 04:42:50 -0600

    adding dependencies

---
+++ src/main/java/group/crudgui/Main.java
@@ -0,0 +1,11 @@
+package group.crudgui;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        BCNApplication.main(args);
+    }
+
+}
